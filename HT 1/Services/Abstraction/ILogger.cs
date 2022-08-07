41b27e410c42ba4bce6794974b935061eead8fab
Alex Svyrydov <oleksandr.svyrydov@nure.ua>
2022-08-07 21:08:10 +0300

    Init

---
+++ HT 1/Services/Abstraction/ILogger.cs
@@ -0,0 +1,19 @@
+﻿namespace HT_1.Services.Abstraction;
+
+public interface ILogger
+{
+	void Info(string message);
+
+	void Error(string message);
+
+	void UnexpercedFileExtention(string path);
+
+	void Start(string path);
+	void ParserFound(string path);
+	void FileRead(string path);
+	void Parsered(string path);
+	void Grouped(string path);
+	void Ended(string path);
+
+	void MidnightReportDone();
+}
---
+++ HT 1/Services/Templates/OutputTemplates.cs
@@ -0,0 +1,37 @@
+﻿namespace HT_1.Services.Templates;
+
+public static class OutputTemplates
+{
+	public static string MetaLogTemplate =
+		"parsed_files: {0}\n" +
+		"parsed_lines: {1}\n" +
+		"found_errors: {2}\n" +
+		"invalid_files: [{3}]\n";
+
+	public static string UnexpercedFileExtention =
+		"Unexperced File Extention: {0} ({1})";
+
+	public static string Start =
+		"Start: {0}";
+
+	public static string ParserFound =
+		"Parser Found: {0}";
+
+	public static string FileRead =
+		"File Read: {0}";
+
+	public static string Parsered =
+		"Parsered: {0}";
+
+	public static string Grouped =
+		"Grouped: {0}";
+
+	public static string Ended =
+		"Ended: {0}";
+
+	public static string MidnightReportDone =
+		"Midnight Report Done";
+
+	public static string TimeFormat =
+		" | Time: {0}";
+}
